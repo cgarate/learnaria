bc5fd2b15875d6b2f23073942a65e481544b7253
Carlos Garate <carlos.garate@rangle.io>
2018-10-04 16:18:04 -0400

    Uncommented if in join.lib.js

--- join.js
+++ join.js
@@ -47,16 +47,16 @@ var showBlock = function(className){
   }
 
 window.onload = function () {
-    if($s("country").value == ''){ 
+    if($s("country").value == ''){
             $s("country").value = "usa";
     }
-    setCountry(); 
+    setCountry();
     joinForm = new JoinForm();
     joinForm.setMessages();
     $s("country").onchange = setCountry;
     $s("join").onclick = joinClick;
     $s("reset_form").onclick = resetClick;
-    
+
     var a = document.getElementsByClassName("fake");
     for(i=0 ; i<a.length ; i++){
         a[i].addEventListener('click', function(e) {
@@ -65,5 +65,5 @@ window.onload = function () {
                 alert("fake link");
             }
         });
-    } 
+    }
 }
